9d36d022a7a7ccea24270c135d89e1c3e2ceaa01
Bledar Muca <b.muca@inera.it>
2016-10-20 12:28:23 +0200

    This resolves #3

--- app/scripts/islightbox.js
+++ app/scripts/islightbox.js
@@ -7,22 +7,10 @@
             escKey: true,
             /*
             navKey: true,
-            closeTip: 'tip-l-fade',
-            closeTipText: 'Close',
-            prevTip: 'tip-t-fade',
-            prevTipText: 'Previous',
-            nextTip: 'tip-t-fade',
-            nextTipText: 'Next',
-            callbackInit: function() {},
-            callbackBeforeOpen: function() {},
-            callbackAfterOpen: function() {},
-            callbackBeforeClose: function() {},
-            callbackAfterClose: function() {},
             callbackError: function() {},
             callbackPrev: function() {},
             callbackNext: function() {},
-            callbackAfterShow: function() {},
-            errorMessage: 'Error loading content.'*/
+            */
         };
 
         var options = $.extend({}, defaults, config);
@@ -30,7 +18,7 @@
         $.isLightBox.init($(this), options);
     };
 
-    $.extend({
+    var plugin = {
         isLightBox: {
             init: function($obj, options) {
                 var $isLightBox = this;
@@ -38,10 +26,44 @@
                 $obj.on('click', function(e) {
                     e.preventDefault();
 
+                    $isLightBox.events.callInit($obj);
+
                     var elementPath = $(this).attr('href');
 
-                    $isLightBox.createIsLightBox(elementPath, options);
+                    $isLightBox.createIsLightBox(elementPath, options, $obj);
                 });
+            },
+            events: {
+                triggerEvent: function(event, $obj) {
+                    $obj = (($obj === undefined) ? $(document) : $obj);
+
+                    $obj.trigger('islb:' + event);
+                },
+                callInit: function($obj) {
+                    plugin.isLightBox.events.triggerEvent('init', $obj);
+                },
+                callOpen: {
+                    triggerEvent: function(event, $obj) {
+                        plugin.isLightBox.events.triggerEvent('open:' + event, $obj);
+                    },
+                    before: function($obj) {
+                        this.triggerEvent('before', $obj);
+                    },
+                    after: function($obj) {
+                        this.triggerEvent('after', $obj);
+                    }
+                },
+                callClose: {
+                    triggerEvent: function(event, $obj) {
+                        plugin.isLightBox.events.triggerEvent('close:' + event, $obj);
+                    },
+                    before: function($obj) {
+                        this.triggerEvent('before', $obj);
+                    },
+                    after: function($obj) {
+                        this.triggerEvent('after', $obj);
+                    }
+                }
             },
             setStyles: function(options) {
                 var $styles = '<style type="text/css" id="islb-styles">.islb {background-color: ' + options.background + '; transition: opacity ' + options.revealSpeed / 1000 + 's ease-in-out;} .islb.islb-active {transition: opacity ' + options.revealSpeed / 1000 + 's ease-in-out;}</style>';
@@ -50,7 +72,7 @@
 
                 $('head').append($styles);
             },
-            createIsLightBox: function(elementPath, options) {
+            createIsLightBox: function(elementPath, options, $obj) {
                 var $isLightBox = this;
 
                 $isLightBox.setStyles(options);
@@ -75,33 +97,47 @@
 
                 $('body').append($islb);
 
+                $isLightBox.events.callOpen.before($obj);
+
                 setTimeout(function() {
                     $islb.addClass('islb-active');
+
+                    $isLightBox.events.callOpen.after($obj);
                 }, 100);
 
                 if (options.overlayClose) {
                     $islb.on('click', function(e) {
                         if(($(e.target).closest('.islb-media').length == 0 || $(e.target).hasClass('islb-media')) && (!$(e.target).hasClass('islb-next') && !$(e.target).hasClass('islb-prev'))) {
-                            $isLightBox.destroyIsLightBox(options);
+                            $isLightBox.destroyIsLightBox(options, $obj);
                         }
+                    });
+                } else {
+                    $islbClose.on('click', function(e) {
+                        $isLightBox.destroyIsLightBox(options, $obj);
                     });
                 }
 
                 if (options.escKey) {
                     $(document).on('keyup', function(e) {
                         if (e.keyCode == 27) {
-                            $isLightBox.destroyIsLightBox(options);
+                            $isLightBox.destroyIsLightBox(options, $obj);
                         }
                     });
                 }
             },
-            destroyIsLightBox: function(options) {
+            destroyIsLightBox: function(options, $obj) {
+                var $isLightBox = this;
+
+                $isLightBox.events.callClose.before($obj);
+
                 $('.islb').removeClass('islb-active');
 
                 $(document).off('keyup');
 
                 setTimeout(function() {
                     $('.islb').remove();
+
+                    $isLightBox.events.callClose.after($obj);
                 }, options.revealSpeed + 100);
             },
             getElement: {
@@ -113,7 +149,9 @@
                 }
             }
         }
-    });
+    };
+
+    $.extend(plugin);
 })(jQuery);
 
 $(function() {
